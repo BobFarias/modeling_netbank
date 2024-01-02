4df9803c6d9efbd4c9292897d420c853acbc7c46
Bob Farias <bobhenrique9@gmail.com>
2024-01-02 22:33:46 +0000

    [FIX] making sure that the pocket account will have only one account per currency

--- interfaces/lib/account-data.ts
+++ interfaces/lib/account-data.ts
@@ -36,8 +36,30 @@ export interface IAccount  {
 // Regular account interface
 export interface IRegularAccount extends IAccount, ILocalMoney{}
 
-// Interface for account details along with the amount of money
-// Pocket account interface
-export interface IPockeAccount extends IAccount{
-  pockets: Array<IPocketMoney>;
+// Define types for pockets with specific foreign currencies.
+// This is necessary to make sure that the PocketAccount will have just one pocket for each currency
+export type SEKPocket = { amount: number; currency: EForeignCurrencies.SEK };
+export type NOKPocket = { amount: number; currency: EForeignCurrencies.NOK };
+export type USDPocket = { amount: number; currency: EForeignCurrencies.USD };
+export type EURPocket = { amount: number; currency: EForeignCurrencies.EUR };
+
+// Create a union type representing any of the defined pocket types.
+export type AnyPocket = SEKPocket | NOKPocket | USDPocket | EURPocket;
+
+// Define an interface for a pocket account, extending from IAccount.
+// It contains an array of pockets, each of which can be of any defined pocket type.
+export interface IPocketAccount extends IAccount {
+  pockets: AnyPocket[];
+}
+
+// Essential function to add a new pocket to a pocket account
+// It will ensure that the account does not have the currency that they are trying to create
+export function addPocket(account: IPocketAccount, newPocket: AnyPocket): IPocketAccount | Error {
+  // Check if the account already has a pocket with the same currency as the new pocket.
+  if (account.pockets.some(pocket => pocket.currency === newPocket.currency)) {
+    // If a pocket with the same currency exists, return an error.
+    return new Error("A pocket with this currency already exists.");
+  }
+  // If no duplicate currency is found, add the new pocket to the account and return the updated account.
+  return { ...account, pockets: [...account.pockets, newPocket] };
 }
